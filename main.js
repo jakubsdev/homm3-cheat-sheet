5559aa578f40872a742c2b91aa239d9329f524ea
Jakub Świętoniowski <50next50@gmail.com>
2017-10-16 00:10:05 +0100

    Added the rest of artifacts from base game and remade the hover view on left panel

--- main.js
+++ main.js
@@ -5,34 +5,38 @@ itemDef = document.getElementById("itemDef");
 itemPwr = document.getElementById("itemPwr");
 itemKnwl = document.getElementById("itemKnwl");
 itemDescription = document.getElementById("itemDescription");
-var x = -6;
+var x = -104;
 var y = -438;
 function artifacts(){
-	for(var i = 0; i < 94; i++){
+	for(var i = 0; i < 120; i++){
 		item = document.createElement('li');
 		item.id = 'artifact' + i;
 		item.className = "item";
 		item.style.backgroundPosition = x + "px" + " " + y + "px";
 		artifactsGrid.appendChild(item);
 		x -= 49;
-		if(i == 31){
+		if(i == 29){
 			x = -6;
 			y = -498;
 		}
-		else if(i == 62){
+		else if(i == 61){
 			x = -6;
 			y = -558;
+		}
+		else if(i == 92){
+			x = -6;
+			y = -614;
 		}
 		
 		a = document.getElementById('artifact'+i);
         (function (a) {
             a.addEventListener('mouseover', function(){
-				var name;
-				var atk;
-				var def;
-				var pwr;
-				var knwl;
-				var desc;				
+				var name = "";
+				var atk = "";
+				var def = "";
+				var pwr = "";
+				var knwl = "";
+				var desc = "";				
                 switch(a.id){
 					case "artifact0":
 						name = "Centaur's Axe";
@@ -43,12 +47,20 @@ function artifacts(){
 						desc = "This right handed weapon increases your Attack skill by +2."
 				}
 				itemName.innerHTML = name;
-				itemAtk.innerHTML = atk;
-				itemDef.innerHTML = def;
-				itemPwr.innerHTML = pwr;
-				itemKnwl.innerHTML = knwl;
+				itemAtk.innerHTML = "A: " + atk;
+				itemDef.innerHTML = "D: " + def;
+				itemPwr.innerHTML = "P: " + pwr;
+				itemKnwl.innerHTML = "K: " + knwl;
 				itemDescription.innerHTML = desc;
             });
+			a.addEventListener('mouseout', function(){
+				itemName.innerHTML = "";
+				itemAtk.innerHTML = "";
+				itemDef.innerHTML = "";
+				itemPwr.innerHTML = "";
+				itemKnwl.innerHTML = "";
+				itemDescription.innerHTML = "";
+			});
         })(a);
 	}
 	
