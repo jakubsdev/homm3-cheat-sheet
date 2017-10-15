f383bc1a86dda7cdb0e64301dc8adcc270faf296
Jakub Świętoniowski <50next50@gmail.com>
2017-10-15 15:48:35 +0100

    Added event listener and changed divs to list

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-mainPanel = document.getElementById("mainPanel");
+artifactsList = document.querySelector("#mainPanel > ul");
 var x = -6;
 var y = -438;
 function artifacts(){
-	for(var i = 0; i < 15; i++){
-		item = document.createElement('div');
-		item.className = 'item';
+	for(var i = 0; i < 32; i++){
+		item = document.createElement('li');
+		item.id = 'artifact' + i;
+		item.className = "item";
 		item.style.backgroundPosition = x + "px" + " " + y + "px";
-		mainPanel.appendChild(item);
+		artifactsList.appendChild(item);
 		x -= 49;
+		
+		a = document.getElementById('artifact'+i);
+        (function (a) {
+            a.addEventListener('click', function(){
+                console.log(a);
+            });
+        })(a);
 	}
-};
+}
 artifacts();
-
-//	background-position: -6px -438px;
